265a03909a400f2a9f67f7cc6bf0ca799bb2c7a2
MuriloSdaSilva <murilor72003@outlook.com>
2023-11-20 11:05:39 -0300

    test: create test open and close page

--- src/test/java/org/example/RegistrationOfDropoutTest.java
+++ src/test/java/org/example/RegistrationOfDropoutTest.java
@@ -1,4 +1,23 @@
 package org.example;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.example.RegistrationOfDropoutPage;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
 
 public class RegistrationOfDropoutTest {
+    private WebDriver driver;
+    @Test
+    public void shouldOpenRegisterPageTest() throws InterruptedException{
+        WebDriverManager.firefoxdriver().setup();
+        driver = new FirefoxDriver();
+        driver.get("http://localhost:3000/");
+        Thread.sleep(1000);
+        driver.quit();
+    }
+
 }
