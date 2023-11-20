55ced8e5391362074cbcef2e765ad0645891d4ad
MuriloSdaSilva <murilor72003@outlook.com>
2023-11-20 12:23:54 -0300

    refactor: edit RegistrationOfDropoutPage

--- src/test/java/org/example/RegistrationOfDropoutPage.java
+++ src/test/java/org/example/RegistrationOfDropoutPage.java
@@ -2,9 +2,10 @@ package org.example;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
 
 
-public class RegistrationOfDropoutPage {
+public class RegistrationOfDropoutPage{
     protected WebDriver webDriver;
 
     private By nameBy = By.id("txtName");
@@ -14,10 +15,14 @@ public class RegistrationOfDropoutPage {
     private By buttonRegistration = By.id("registrationFormSubmitButton");
     private By buttonList = By.id("bt2");
 
-
+    public RegistrationOfDropoutPage(WebDriver driver){
+        this.webDriver = driver;
+        PageFactory.initElements(driver, this);
+    }
 
     public void SignInPageDropout(WebDriver webDriver){
         this.webDriver = webDriver;
+        webDriver.get("http://localhost:3000/");
         if (!webDriver.getTitle().equals("Bye Poo")) {
             throw new IllegalStateException("This is not Sign In Page," +
                     " current page is: " + webDriver.getCurrentUrl());
