d33fc2bcdc0e0c964c62fafa25de83eda24e4d44
MuriloSdaSilva <murilor72003@outlook.com>
2023-11-20 09:59:34 -0300

    feat: configuring the project

---
+++ src/test/java/org/example/SeleniumTest.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import org.junit.jupiter.api.DisplayName;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+    @Test
+    @DisplayName("Should open and close firefox browser")
+    void shouldOpenAndCloseChromeBrowser() throws InterruptedException {
+        System.setProperty("webdriver.firefox.driver","src/test/resources/drivers/geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://www.google.com");
+        Thread.sleep(1000);
+        driver.quit();
+    }
+}
