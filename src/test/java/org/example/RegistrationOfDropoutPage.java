b0271624ea61963f71f6e89fdd3b000f45995e1c
MuriloSdaSilva <murilor72003@outlook.com>
2023-11-20 11:05:01 -0300

    feat: create page object RegistrationOfDropout

--- src/test/java/org/example/RegistrationOfDropoutPage.java
+++ src/test/java/org/example/RegistrationOfDropoutPage.java
@@ -1,4 +1,27 @@
 package org.example;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+
 public class RegistrationOfDropoutPage {
+    protected WebDriver webDriver;
+
+    private By nameBy = By.id("txtName");
+    private By identifyBy = By.id("txtId");
+    private By reasonBy = By.id("txtReason");
+
+    private By buttonRegistration = By.id("registrationFormSubmitButton");
+    private By buttonList = By.id("bt2");
+
+
+
+    public void SignInPageDropout(WebDriver webDriver){
+        this.webDriver = webDriver;
+        if (!webDriver.getTitle().equals("Bye Poo")) {
+            throw new IllegalStateException("This is not Sign In Page," +
+                    " current page is: " + webDriver.getCurrentUrl());
+        }
+    }
+
 }
