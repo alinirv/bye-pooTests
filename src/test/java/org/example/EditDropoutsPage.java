e30827d5f3f8b47ee7973e4a95fa48a53fe9a06c
Everton Lourenco <promo.everton@gmail.com>
2023-11-23 00:16:29 -0300

    feat: create page object EditDropoutsPage

--- src/test/java/org/example/EditDropoutsPage.java
+++ src/test/java/org/example/EditDropoutsPage.java
@@ -1,4 +1,49 @@
 package org.example;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class EditDropoutsPage {
+
+    protected WebDriver webDriver;
+    //verificar os nomes dos campos
+    @FindBy(id = "txtEditedName")
+    private WebElement editedNameBy;
+
+    @FindBy(id = "txtEditedReason")
+    private WebElement editedReasonBy;
+
+    @FindBy(id = "btnSaveEdit")
+    private WebElement saveEditButton;
+
+    @FindBy(xpath = "//div[@id='editConfirmation']/p")
+    private WebElement editConfirmationMessage;
+
+    public EditDropoutsPage(WebDriver driver) {
+        this.webDriver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public void editarInformacoesDesistencias(WebDriver driver, String novoNome, String novoMotivo) {
+        editedNameBy.clear();
+        editedNameBy.sendKeys(novoNome);
+
+        editedReasonBy.clear();
+        editedReasonBy.sendKeys(novoMotivo);
+
+        saveEditButton.click();
+    }
+
+    public WebElement getEditConfirmationMessage() {
+        return editConfirmationMessage;
+    }
 }
+
+
