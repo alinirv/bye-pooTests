16ddec8b74b552b9951d6fa2f20f49220bbe68fe
alinirv <alini.vicente@yahoo.com.br>
2023-11-28 18:45:01 -0300

    feat: configure page object ListOfDropoustsPage

--- src/test/java/org/example/ListOfDropoutsPage.java
+++ src/test/java/org/example/ListOfDropoutsPage.java
@@ -1,4 +1,23 @@
 package org.example;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
 public class ListOfDropoutsPage {
+    private WebDriver driver;
+
+    public ListOfDropoutsPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void openListPage() {
+        driver.get("http://localhost:3000/Desistentes");
+    }
+
+
+
+
 }
