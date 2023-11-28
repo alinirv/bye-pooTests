7def56fe8b1c33e271633f271e4238ed114d88f4
alinirv <alini.vicente@yahoo.com.br>
2023-11-28 18:07:20 -0300

    feat: configure class ListOfDropoutsTest

--- src/test/java/org/example/ListOfDropoutsTest.java
+++ src/test/java/org/example/ListOfDropoutsTest.java
@@ -1,4 +1,31 @@
 package org.example;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import java.time.Duration;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.time.Duration;
+
 public class ListOfDropoutsTest {
+
+    @BeforeEach
+    public void setUp() {
+        WebDriverManager.firefoxdriver().setup();
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(3000));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+    }
+
+    private WebDriver driver;
+
+
+
+
 }
