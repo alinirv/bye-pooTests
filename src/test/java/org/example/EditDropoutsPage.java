d19f3012c8889fe7a27d8c9c9b5f0096deb9b73b
Everton Lourenco <promo.everton@gmail.com>
2023-11-28 22:11:13 -0300

    feat: adiciona metodos na EditDropoutsPage

--- src/test/java/org/example/EditDropoutsPage.java
+++ src/test/java/org/example/EditDropoutsPage.java
@@ -1,22 +1,31 @@
 package org.example;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+import com.github.javafaker.Faker;
+
+import java.util.List;
+import java.util.Locale;
 
 public class EditDropoutsPage {
 
     protected WebDriver webDriver;
-    //verificar os nomes dos campos
-    @FindBy(id = "txtEditedName")
-    private WebElement editedNameBy;
+    Faker faker = new Faker(new Locale("pt-BR"));
 
-    @FindBy(id = "txtEditedReason")
-    private WebElement editedReasonBy;
+    @FindBy(id = "txtName")
+    private WebElement nameBy;
 
-    @FindBy(id = "btnSaveEdit")
-    private WebElement saveEditButton;
+    @FindBy(id = "txtId")
+    private WebElement idBy;
+    @FindBy(id = "txtReason")
+    private WebElement reasonBy;
+
+    @FindBy(id = "registrationFormSubmitButton")
+    private WebElement buttonRegistration;
 
     @FindBy(xpath = "//div[@id='editConfirmation']/p")
     private WebElement editConfirmationMessage;
@@ -29,14 +38,17 @@ public class EditDropoutsPage {
         PageFactory.initElements(driver, this);
     }
 
-    public void editarInformacoesDesistencias(WebDriver driver, String novoNome, String novoMotivo) {
-        editedNameBy.clear();
-        editedNameBy.sendKeys(novoNome);
+    public void editDropoutInformation(WebDriver driver) {
+        String name = String.valueOf(faker.name().fullName());
+        String identify = "SC0000003";
+        nameBy.sendKeys(name);
+        idBy.sendKeys(identify);
 
-        editedReasonBy.clear();
-        editedReasonBy.sendKeys(novoMotivo);
+        Select select = new Select(reasonBy);
+        List<WebElement> optionList = select.getOptions();
+        select.selectByValue("Não estava muito bem");
 
-        saveEditButton.click();
+        buttonRegistration.click();
     }
     public WebElement getEditConfirmationMessage() {
 
@@ -44,6 +56,22 @@ public class EditDropoutsPage {
     }
     public WebElement getErrorMessageBox() {
         return errorMessageBox;
+    }
+    public void clikEditStudent(WebElement studentRow){
+        studentRow.findElement(By.cssSelector("svg:nth-child(1)")).click();
+    }
+    public WebElement getId(){
+        return idBy;
+    }
+    public WebElement getName(){
+        return nameBy;
+    }
+    public WebElement getReason(){
+        return reasonBy;
+    }
+
+    public List<WebElement> getStudentRows() {
+        return webDriver.findElements(By.cssSelector(".StudentRow_StudentRow__JhSrj"));
     }
 }
 
